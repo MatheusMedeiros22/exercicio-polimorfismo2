2cc4c83f5a8cfe2e49b1360973c3ad0853271759
Matheus Medeiros <mathmed22@gmail.com>
2022-05-20 15:32:28 -0300

    implementacao

--- application/Program.java
+++ application/Program.java
@@ -1,10 +1,62 @@
 package application;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.Locale;
+import java.util.Scanner;
+
+import entities.ImportedProduct;
+import entities.Product;
+import entities.UsedProduct;
+
 public class Program {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws ParseException {
 		// TODO Auto-generated method stub
-
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		List<Product> list = new ArrayList<>();
+		SimpleDateFormat sdf =  new SimpleDateFormat("dd/MM/yyyy");
+		
+		System.out.println("Enter the number of products: ");
+		int numberProducts = sc.nextInt();
+		
+		for(int i = 0; i < numberProducts; i++) {
+			System.out.println("Product #" + (i+1) + " data:");
+			System.out.println("Common, used or imported (c/u/i)?");
+			char type = sc.next().charAt(0);
+			sc.nextLine();
+			System.out.print("Name:");
+			String name = sc.nextLine();
+			System.out.print("Price:");
+			double price = sc.nextDouble();
+			if(type == 'i') {
+				System.out.println("Customs fee: ");
+				double customsFee = sc.nextDouble();
+				list.add(new ImportedProduct(name, price, customsFee));
+			}else if(type == 'u') {
+				System.out.println("Manufacture date: ");
+				Date manufactureDate = sdf.parse(sc.next());
+				list.add(new UsedProduct(name, price, manufactureDate));
+			}else {
+				list.add(new Product(name, price));
+			}
+		
+		}
+		
+		System.out.println();
+		System.out.println("PRICE TAGS:");
+		for(Product prod : list) {
+			System.out.println(prod.priceTag());
+		}
+		
+		
+		
+		
+		sc.close();
 	}
 
 }
--- entities/UsedProduct.java
+++ entities/UsedProduct.java
@@ -1,5 +1,34 @@
 package entities;
 
-public class UsedProduct {
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
+public class UsedProduct extends Product {
+	private Date manufactureDate;
+	
+	private static SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+	
+	public UsedProduct() {
+		
+	}
+
+	public UsedProduct(String name, Double price, Date manufactureDate) {
+		super(name, price);
+		this.manufactureDate = manufactureDate;
+	}
+	
+	@Override
+	public String priceTag() {
+		return super.priceTag() + " - Manufacture date: " + sdf.format(manufactureDate);
+	}
+	
+	public Date getManufactureDate() {
+		return manufactureDate;
+	}
+
+	public void setManufactureDate(Date manufactureDate) {
+		this.manufactureDate = manufactureDate;
+	}
+	
+	
 }
